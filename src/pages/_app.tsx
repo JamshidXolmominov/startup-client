4aafec5d273d9abcd865ba48b39ace54817978a8
JamshidXolmominov <xolmominovj2023@gmail.com>
2023-03-30 15:14:31 +0300

    Higher-Order Component, Header & Sidebar

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../styles/globals.css';
+import '@fontsource/roboto';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../config/theme';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+	return (
+		<ChakraProvider theme={theme}>
+			<Component {...pageProps} />;
+		</ChakraProvider>
+	);
+}
