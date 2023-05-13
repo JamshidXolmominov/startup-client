b6ed1f0cdadc9d89ab1469d9318d32275ba6be84
JamshidXolmominov <xolmominovj2023@gmail.com>
2023-05-13 13:06:36 +0300

    Code review & Internalization

--- src/components/instructor-courses-card/instructor-courses-card.tsx
+++ src/components/instructor-courses-card/instructor-courses-card.tsx
@@ -1,17 +1,17 @@
 import { Box, Button, Divider, Flex, Heading, HStack, Icon, Stack, Text } from '@chakra-ui/react';
 import Image from 'next/image';
 import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 import { AiOutlineClockCircle } from 'react-icons/ai';
-import { BsTrash } from 'react-icons/bs';
 import { CiViewList } from 'react-icons/ci';
-import { FiEdit2 } from 'react-icons/fi';
-import { HiOutlineStatusOnline } from 'react-icons/hi';
 import { SiGoogleanalytics } from 'react-icons/si';
 import { VscOpenPreview } from 'react-icons/vsc';
 import { loadImage } from 'src/helpers/image.helper';
 import { InstructoCoursesCardProps } from './instructor-courses-card.props';
 
 const InstructorCoursesCard: FC<InstructoCoursesCardProps> = ({ item }): JSX.Element => {
+	const { t } = useTranslation();
+
 	return (
 		<HStack key={item.title} p={5} boxShadow={'dark-lg'} mt={5} borderRadius={'lg'}>
 			<Stack spacing={5} w={'70%'}>
@@ -22,11 +22,15 @@ const InstructorCoursesCard: FC<InstructoCoursesCardProps> = ({ item }): JSX.Ele
 				<HStack>
 					<Flex align={'center'} gap={1}>
 						<Icon as={CiViewList} />
-						<Text>{item.lessonCount} lesson</Text>
+						<Text>
+							{item.lessonCount} {t('lessons', { ns: 'courses' })}
+						</Text>
 					</Flex>
 					<Flex align={'center'} gap={1}>
 						<Icon as={AiOutlineClockCircle} />
-						<Text>{item.totalHour} hours</Text>
+						<Text>
+							{item.totalHour} {t('hour', { ns: 'courses' })}
+						</Text>
 					</Flex>
 					<Flex align={'center'} gap={1}>
 						<Icon as={SiGoogleanalytics} />
@@ -35,10 +39,9 @@ const InstructorCoursesCard: FC<InstructoCoursesCardProps> = ({ item }): JSX.Ele
 				</HStack>
 				<Divider />
 				<HStack>
-					<Button rightIcon={<VscOpenPreview />}>Preview</Button>
-					<Button rightIcon={<FiEdit2 />}>Edit</Button>
-					<Button rightIcon={<BsTrash />}>Delete</Button>
-					<Button rightIcon={<HiOutlineStatusOnline />}>Status</Button>
+					<Button rightIcon={<VscOpenPreview />} w={'full'} h={16} colorScheme={'facebook'}>
+						{t('preview', { ns: 'instructor' })}
+					</Button>
 				</HStack>
 			</Stack>
 			<Box w={'30%'} h={'300px'} position={'relative'}>
